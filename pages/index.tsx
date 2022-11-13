46e0a58ba621ac723d13dfe384915e48c547dc95
kkata <14849535+kkata@users.noreply.github.com>
2022-11-13 18:21:14 +0900

    getServerSideProps and getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import type { GetStaticPropsResult } from "next";
+import { Data } from "../type";
 
-export default function Home() {
+const Home = ({ events }: { events: Data[] }) => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
-}
+};
+
+// ref. https://blog.webcreativepark.net/2022/01/21-140314.html
+export const getStaticProps = async (): Promise<
+  GetStaticPropsResult<{ events: Data[] }>
+> => {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
+};
+
+export default Home;
